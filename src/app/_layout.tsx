a66d33013ecd19dc614bc8617256c0f279a84356
Felipe Reis <felipematheus17@hotmail.com>
2025-09-06 14:38:08 -0300

    :tada: initial commit

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,37 @@
-import "../global.css";
-import { Slot } from "expo-router";
+import { Tabs } from "expo-router";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
-export default function Layout() {
-  return <Slot />;
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: "hls(var(--primary))" }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ size, color, focused }) => (
+            <MaterialIcons
+              size={size}
+              name={focused ? "home" : "home-filled"}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="tracks/index"
+        options={{
+          title: "Rotas",
+          headerShown: false,
+          tabBarIcon: ({ size, color, focused }) => (
+            <MaterialIcons
+              size={size}
+              name={focused ? "map" : "route"}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
